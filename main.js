c25967490e2b30c01286304fae23d1e27df85050
Elliot Brock <ebrock@Jeffreys-MacBook-Pro.local>
2016-09-02 17:06:12 -0400

    always include your trailing slashes kiddos

--- main.js
+++ main.js
@@ -2,11 +2,10 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var cookieParser = require('cookie-parser');
 var appRoutes = require('./routes/app');
-var apiRoutes = require('./routes/api');
+// var apiRoutes = require('./routes/api');
 
 var app = express();
 
@@ -14,13 +13,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'app/prod/hbs'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'app/prod/img', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'app/prod/img', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app/prod/')));
 
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -29,7 +27,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use('/api', apiRoutes);
+// app.use('/api', apiRoutes);
 app.use('/', appRoutes);
 
 // catch 404 and forward to error handler
